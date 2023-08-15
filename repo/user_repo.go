1a29e35f6838e7fc8706a386681ea5e67170817f
yudi <syahyudi996@gmail.com>
2023-08-15 13:27:04 +0700

    [FEATURE] avatar update repo

--- repo/user_repo.go
+++ repo/user_repo.go
@@ -10,7 +10,9 @@ import (
 
 type UserRepo interface {
 	RegisterUser(*model.UserModel) error
-	FindByEmail(string) (*model.UserModel, error)
+	FindByEmail(string) (model.UserModel, error)
+	GetUserByID(int) (model.UserModel, error)
+	UpdateAvatar(int, *model.UserModel) error
 }
 
 type userRepoImpl struct {
@@ -29,19 +31,45 @@ func (u *userRepoImpl) RegisterUser(user *model.UserModel) error{
 	return nil
 }
 
-func (u *userRepoImpl) FindByEmail(email string) (*model.UserModel, error) {
-	query := "SELECT id, name, email FROM users WHERE email = $1"
+func (u *userRepoImpl) FindByEmail(email string) (model.UserModel, error) {
+	query := utils.FIND_BY_EMAIL
 	row := u.db.QueryRow(query, email)
 
 	var user model.UserModel
 	err := row.Scan(&user.ID, &user.Name, &user.Email)
 	if err != nil {
-		return nil, fmt.Errorf("error fetching user by email: %w", err)
+		return user, fmt.Errorf("error fetching user by email: %w", err)
 	}
 
-	return &user, nil
+	return user, nil
 }
 
+func (u *userRepoImpl) GetUserByID(id int) (model.UserModel, error) {
+	query := utils.GET_USER_BY_ID
+	row := u.db.QueryRow(query, id)
+
+	var user model.UserModel
+	err := row.Scan(user.ID)
+	if err != nil {
+		return user, fmt.Errorf("Error an userRepoImpl.GetUserById %d", err)
+	}
+
+	return user, nil
+}
+
+func (u *userRepoImpl) UpdateAvatar(id int, cust *model.UserModel) error {
+	_, err := u.GetUserByID(id)
+	if err != nil {
+		return fmt.Errorf("Error an userRepoImpl.UpdateAvatar %w", err)
+	}
+
+	query := "UPDATE users SET "
+	_, err = u.db.Exec(query, &cust.AvatarFileName, &cust.ID)
+	if err != nil {
+		return fmt.Errorf("Error an userRepoImpl.UpdateAvatar %w", err)
+	}
+	return nil
+}
 func NewUserRepo(db *sql.DB) UserRepo{
 	return &userRepoImpl{
 		db: db,
--- utils/constant.go
+++ utils/constant.go
@@ -4,4 +4,5 @@ const (
 	// REGISTER USER
 	REGISTER_USER = "INSERT INTO users(name, accoupation, email, password_hash, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6)"
 	FIND_BY_EMAIL = "SELECT id, name, email FROM users WHERE email = $1"
+	GET_USER_BY_ID = "SELECT id from users WHERE id = $1"
 )
