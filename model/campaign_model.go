64bd008f279882ff9f58e9eb09c711dffb161a43
yudi <syahyudi996@gmail.com>
2023-08-21 13:13:42 +0700

    [IMPROVE] campign model

--- model/campaign_model.go
+++ model/campaign_model.go
@@ -1,1 +1,27 @@
 package model
+
+import "time"
+
+type CampaignModel struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreateAt         time.Time
+	UpdateAt		 time.Time
+}
+
+type CampaignImager struct{
+	ID int
+	CampingID int
+	FileName string
+	IsPrimary int
+	CreateAt time.Time
+	UpdateAt time.Time
+}
